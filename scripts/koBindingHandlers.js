b466d5e67afdd78632257b6b06ea4299be0d56c9
artin.phares <artin.phares@gmail.com>
2014-08-06 13:47:10 +0400

    #3 Implemented sliding effect when switching slides. Added slideShow KO binding.

--- scripts/koBindingHandlers.js
+++ scripts/koBindingHandlers.js
@@ -1,15 +1,87 @@
 // ---------------------  CUSTOM KO BINDING HANDLERS -------------------------------
-ko.bindingHandlers.editableHTML = {
-    init: function(element, valueAccessor) {
-        $(element).on('keyup', function() {
+
+/* Syncs innerHTML property of the element with observable. */
+ko.bindingHandlers.innerHTML = {
+    init: function (element, valueAccessor) {
+        $(element).on('keyup', function () {
             var value = valueAccessor();
             value(this.innerHTML);
         });
     },
-    update: function(element, valueAccessor) {
+    update: function (element, valueAccessor) {
         var value = ko.utils.unwrapObservable(valueAccessor());
         if (element.innerHTML != value) {
             element.innerHTML = value;
+        }
+    }
+};
+
+/** Manages visibility of element with animated slide effect.
+ Shows element by sliding from outside the screen to target position,
+ hides element by sliding from target position to outside the screen.
+ Binding properties:
+ @slideShow - true to show, false to hide.
+ @slideTargetPosition - id of element which top-left corner will indicate
+ target position for sliding when showing.
+ @slideDirection - > 0 to move from right to left, < 0 - vise versa,
+ 0 - set visibility without animation.
+ */
+ko.bindingHandlers.slideShow = {
+    update: function (element, valueAccessor, allBindings) {
+        var makeVisible = ko.utils.unwrapObservable(valueAccessor());
+
+        // Do not do anything if element already is in desirable visibility state.
+        var currentlyVisible = $(element).is(":visible");
+        if (currentlyVisible == makeVisible) {
+            return;
+        }
+
+        // Setup slide properties.
+        var targetPositionElement = allBindings.get('slideTargetPosition');
+        if (!targetPositionElement) {
+            throw Error("Target position not specified");
+        }
+        var slideDirection = allBindings.get('slideDirection');
+
+        var targetPosition = targetPositionElement.getBoundingClientRect();
+        var elementWidth = $(element).outerWidth();
+        var duration = 500;
+        var method = "swing";
+
+        // Show or hide element by sliding if we know direction,
+        // otherwise just set visibility.
+        if (slideDirection == 0) {
+            if (makeVisible) {
+                $(element).css({top: targetPosition.top, left: targetPosition.left});
+            }
+
+            $(element).toggle(makeVisible);
+        }
+        else {
+            if (makeVisible) {                        // Show
+                var startPosition =
+                { top: targetPosition.top,
+                    left: slideDirection > 0
+                        ? $(window).width() + elementWidth // start from right side.
+                        : -(elementWidth)                  // .. or from left side.
+                };
+
+                $(element).css({top: startPosition.top, left: startPosition.left });
+                $(element).show();
+                $(element).animate({left: targetPosition.left}, duration, method)
+            }
+            else {                                    // Hide
+                startPosition =
+                { left: slideDirection > 0
+                    ? -(elementWidth)
+                    : $(window).width() + elementWidth
+                };
+
+                $(element).animate({left: startPosition.left}, duration, method,
+                    function () {
+                        $(element).hide()
+                    });
+            }
         }
     }
 };
--- scripts/viewModels/Presentation.js
+++ scripts/viewModels/Presentation.js
@@ -5,6 +5,32 @@ var Presentation = function () {
 
     this.CurrentSlide = ko.observable();
 
+    this.PreviousSlideIndex = ko.observable();
+    this.CurrentSlideIndex = ko.observable();
+
+    this.CurrentSlide.subscribe(function (oldSlide) {
+        if (!oldSlide)
+        {
+            // If there were not any slide before (i.e. initial load),
+            // just empty the index, so we will equal it to new index later.
+            self.PreviousSlideIndex(null);
+        }
+        else {
+            var oldSlideIndex = self.Slides().indexOf(oldSlide);
+            self.PreviousSlideIndex(oldSlideIndex < 0 ? Number.MAX_VALUE : oldSlideIndex);
+        }
+    }, null, "beforeChange");
+
+    this.CurrentSlide.subscribe(function (newSlide) {
+        var currentIndex = self.Slides().indexOf(newSlide);
+        self.CurrentSlideIndex(currentIndex);
+
+        if (self.PreviousSlideIndex() == null)
+        {
+            self.PreviousSlideIndex(currentIndex)
+        }
+    }, null, "change");
+
     this.CurrentSlideNumber = ko.computed(function () {
         var currentNumber = self.Slides().indexOf(self.CurrentSlide()) + 1;
         var totalNumber = self.Slides().length;
@@ -14,6 +40,7 @@ var Presentation = function () {
     this.AddNewSlide = function () {
         var newSlide = new Slide("new slide");
         this.Slides.push(newSlide);
+
         this.CurrentSlide(newSlide);
     }
 
@@ -25,6 +52,7 @@ var Presentation = function () {
         var currentIndex = this.Slides.indexOf(this.CurrentSlide());
         this.Slides.splice(currentIndex, 1);
 
+        // Go to previous slide.
         var currentIndex = --currentIndex >= 0 ? currentIndex : 0;
         this.CurrentSlide(this.Slides()[currentIndex]);
     }
